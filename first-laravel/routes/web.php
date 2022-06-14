43ddc4887a456d452012cf7729ccd7bf0fe275f6
bambinaCh <chaimaa.jarite@gmail.com>
2022-06-14 15:40:05 +0200

    message controller, master blade

--- first-laravel/routes/web.php
+++ first-laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/messages', function () {
+    return view('messages');
+ });
+ 
+ Route::get('/messages', [MessageController::class, 'showAll']);
